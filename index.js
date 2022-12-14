f9007a0c93681ae156602b42e4a1d20763adc017
Ankit jain <52852571+ankitjain091997@users.noreply.github.com>
2022-12-15 00:07:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 
-
+//Create Connection
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
-  database: 'crud_db1'
+  password: 'QAZwsx@123',
+  database: 'crud_db'
 });
 
-
+//connect to database
 conn.connect((err) =>{
   if(err) throw err;
   console.log('Mysql Connected...');
@@ -33,44 +33,50 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //set folder public as static folder for static file
 app.use('/assets',express.static(__dirname + '/public'));
 
-
+//route for homepage
 app.get('/',(req, res) => {
   let sql = "SELECT * FROM product1";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.render('product_view' ,{
-      results: results
-    }); 
+   // ------ for return json response --------------------------------
+    res.send(results);
+    // ------ for return view response --------------------------------
+    // res.render('product_view',{
+    //   results: results 
+    // });
   });
 });
 
-
+//route for insert data
 app.post('/save',(req, res) => {
-  let data = {name: req.body.name, price: req.body.price};
+  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
   let sql = "INSERT INTO product1 SET ?";
   let query = conn.query(sql, data,(err, results) => {
     if(err) throw err;
     res.redirect('/');
+   
   });
 });
 
-
+//route for update data
 app.post('/update',(req, res) => {
-  let sql = "UPDATE product1 SET name='"+req.body.name+"', price='"+req.body.price+"' WHERE id="+req.body.id;
+  let sql = "UPDATE product1 SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.product_id;
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
     res.redirect('/');
   });
 });
 
-
+//route for delete data
 app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product1 WHERE id="+req.body.id+"";
+  let sql = "DELETE FROM product1 WHERE product_id="+req.body.product_id+"";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
       res.redirect('/');
   });
 });
 
-
-app.listen(3000, () => console.log('Server is running at port 3000'));
+//server listening
+app.listen(4200, () => {
+  console.log('Server is running at port 4200');
+});
